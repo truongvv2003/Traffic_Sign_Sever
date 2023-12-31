1dab743c6eec123c76e5d08b6c6f3ef10fd114ce
Võ Viết Trường <voviettruong141003@gmail.com>
2023-12-31 08:55:02 +0700

    English Database

--- src/seeders/20231128091923-sign.js
+++ src/seeders/20231128091923-sign.js
@@ -7,282 +7,282 @@ module.exports = {
       [
         {
           code: "DP.135",
-          content: "Biển báo hết tất cả các lệnh cấm",
+          content: "Sign indicating the end of all prohibitions",
           url: "https://i.imgur.com/eyDKz4u.png"
         },
         {
           code: "P.102",
-          content: "Biển báo cấm đi ngược chiều",
+          content: "No entry sign",
           url: "https://i.imgur.com/w6lRRQo.png"
         },
         {
           code: "P.103a",
-          content: "Biển báo cấm ô tô",
+          content: "No entry for cars",
           url: "https://i.imgur.com/FfqCzSS.png"
         },
         {
           code: "P.103b",
-          content: "Biển báo cấm xe ô tô rẽ trái",
+          content: "No left turn for cars",
           url: "https://i.imgur.com/OqHHGSo.png",
         },
         {
           code: "P.103c",
-          content: "Biển báo cấm xe ô tô rẽ phải",
+          content: "No right turn for cars",
           url: "https://i.imgur.com/0uo9s8M.png",
         },
         {
           code: "P.104",
-          content: "Biển báo cấm mô tô",
+          content: "No entry for motorcycles",
           url: "https://i.imgur.com/aLX7y2E.png"
         },
         {
           code: "P.106a",
-          content: "Biển báo cấm xe tải",
+          content: "No entry for trucks",
           url: "https://i.imgur.com/UBXTZsv.png"
         },
         {
           code: "P.106b",
-          content: "Biển báo cấm ô tô tải có khối lượng chuyên chở lớn hơn giới hạn",
+          content: "No entry for trucks with a weight limit exceeding the specified limit",
           url: "https://i.imgur.com/wVPEAv8.png"
         },
         {
           code: "P.107a",
-          content: "Biển báo cấm ô tô khách",
+          content: "No entry for buses",
           url: "https://i.imgur.com/TRXY7Fs.png"
         },
         {
           code: "P.112",
-          content: "Biển báo cấm người đi bộ",
+          content: "No pedestrians",
           url: "https://i.imgur.com/b9BoUZE.png"
         },
         {
           code: "P.115",
-          content: "Biển báo hạn chế trọng lượng xe",
+          content: "Weight limit restriction",
           url: "https://i.imgur.com/ZlF7chN.png"
         },
         {
           code: "P.117",
-          content: "Biển báo hạn chế chiều cao",
+          content: "Height limit restriction",
           url: "https://i.imgur.com/pkNXyfO.png"
         },
         {
           code: "P.123a",
-          content: "Biển báo cấm rẽ trái",
+          content: "No left turn",
           url: "https://i.imgur.com/T0f6SOI.png"
         },
         {
           code: "P.123b",
-          content: "Biển báo cấm rẽ phải.",
+          content: "No right turn",
           url: "https://i.imgur.com/4uGpZ5c.png"
         },
         {
           code: "P.124a",
-          content: "Biển báo cấm quay đầu xe.",
+          content: "No U-turn",
           url: "https://i.imgur.com/AqAfHvV.png"
         },
         {
           code: "P.124b",
-          content: "Biển báo cấm ô tô quay đầu xe",
+          content: "No U-turn for cars",
           url: "https://i.imgur.com/X2gJeCb.png"
         },
         {
           code: "P.124c",
-          content: "Biển báo cấm rẽ và quay xe",
+          content: "No left or U-turn",
           url: "https://i.imgur.com/6b60Hwe.png"
         },
         {
           code: "P.125",
-          content: "Biển báo cấm vượt",
+          content: "No overtaking",
           url: "https://i.imgur.com/2VAnCHQ.png"
         },
         {
           code: "P.127",
-          content: "Biển báo giới hạn tốc độ.",
+          content: "Speed limit",
           url: "https://i.imgur.com/R94rsMz.png"
         },
         {
           code: "P.128",
-          content: "Biển báo cấm sử dụng còi",
+          content: "No horn usage",
           url: "https://i.imgur.com/NCYR8uB.png"
         },
         {
           code: "P.130",
-          content: "Biển báo cấmIdừng và đỗ xe..",
+          content: "No stopping and parking",
           url: "https://i.imgur.com/FZkJWq5.png"
         },
         {
           code: "P.131a",
-          content: "Biển báo cấm đỗ xe.",
+          content: "No parking",
           url: "https://i.imgur.com/um7ATk0.png"
         },
         {
           code: "P.137",
-          content: "Biển báo cấm rẽ trái và rẽ phải",
+          content: "No left or right turn",
           url: "https://i.imgur.com/mB8UCJs.png"
         },
         {
           code: "R.301c",
-          content: "Biển báo các phương tiện chỉ được rẽ trái",
+          content: "Only left turn allowed",
           url: "https://i.imgur.com/nyE9ydO.png"
         },
         {
           code: "R.301d",
-          content: "Biển báo các phương tiện chỉ đươc rẽ phải.",
+          content: "Only right turn allowed",
           url: "https://i.imgur.com/zyDVtEq.png"
         },
         {
           code: "R.301e",
-          content: "Biển báo các phương tiện chỉ được rẽ trái.",
+          content: "Only left turn allowed",
           url: "https://i.imgur.com/UxTz01d.png"
         },
         {
           code: "R.302a",
-          content: "Biển báo hướng phải đi vòng chướng ngại vật qua phải.",
+          content: "Right direction for obstacle avoidance",
           url: "https://i.imgur.com/Bn2CUIf.png"
         },
         {
           code: "R.302b",
-          content: "Biển báo hướng phải đi vòng chướng ngại vật qua trái.",
+          content: "Left direction for obstacle avoidance",
           url: "https://i.imgur.com/CY6y4kx.png"
         },
         {
           code: "R.303",
-          content: "Biển báo nơi giao nhau chạy theo vòng xuyến.",
+          content: "Intersection running in a roundabout",
           url: "https://i.imgur.com/4OVsSfl.png"
         },
         {
           code: "R.407a",
-          content: "Biển báo đường một chiều.",
+          content: "One-way street",
           url: "https://i.imgur.com/lBV2wgi.png"
         },
         {
           code: "R.409",
-          content: "Biển báo chỗ quay xe",
+          content: "U-turn spot",
           url: "https://i.imgur.com/JtLCIOG.png"
         },
         {
           code: "R.425",
-          content: "Biển báo bệnh viện",
+          content: "Hospital",
           url: "https://i.imgur.com/cL7lBLt.png"
         },
         {
           code: "R.434",
-          content: "Biển báo bến xe buýt",
+          content: "Bus station",
           url: "https://i.imgur.com/CTZBr5O.png"
         },
         {
           code: "W.201a",
-          content: "Biển báo chỗ ngoặt nguy hiểm vòng bên trái.",
+          content: "Sharp left turn ahead",
           url: "https://i.imgur.com/WYkrtnK.png"
         },
         {
           code: "W.201b",
-          content: "Biển báo chỗ ngoặt nguy hiểm vòng bên phải.",
+          content: "Sharp right turn ahead",
           url: "https://i.imgur.com/Ok6jIIf.png"
         },
         {
           code: "W.202a",
-          content: "Biển báo nhiều chỗ ngoặt nguy hiểm liên tiếp, chỗ đầu tiên sang trái",
+          content: "Multiple consecutive sharp turns, first to the left",
           url: "https://i.imgur.com/ppHSLgJ.png"
         },
         {
           code: "W.202b",
-          content: "Biển báo nhiều chỗ ngoặt nguy hiểm liên tiếp, chỗ đầu tiên sang phải",
+          content: "Multiple consecutive sharp turns, first to the right",
           url: "https://i.imgur.com/ZBsopja.png"
         },
         {
           code: "W.203b",
-          content: "Biển báo đường bị hẹp về phía trái.",
+          content: "Road narrows to the left",
           url: "https://i.imgur.com/if8Rr2c.png"
         },
         {
           code: "W.203c",
-          content: "Biển báo đường bị hẹp về phía phải.",
+          content: "Road narrows to the right",
           url: "https://i.imgur.com/ML7CC83.png"
         },
         {
           code: "W.205a",
-          content: "Biển báo đường giao nhau.",
+          content: "Crossroads",
           url: "https://i.imgur.com/02n3Dm5.png"
         },
         {
           code: "W.205b",
-          content: "Biển báo đường giao nhau.",
+          content: "Crossroads",
           url: "https://i.imgur.com/DZxxh1Q.png"
         },
         {
           code: "W.205d",
-          content: "Biển báo đường giao nhau.",
+          content: "Crossroads",
           url: "https://i.imgur.com/rpabQkf.png"
         },
         {
           code: "W.207a",
-          content: "Biển báo giao nhau với đường không ưu tiên..",
+          content: "Crossroads with no priority",
           url: "https://i.imgur.com/NpWf8R3.png"
         },
         {
           code: "W.207b",
-          content: "Biển báo giao nhau với đường không ưu tiên..",
+          content: "Crossroads with no priority",
           url: "https://i.imgur.com/I9aky8n.png"
         },
         {
           code: "W.207c",
-          content: "Biển báo giao nhau với đường không ưu tiên..",
+          content: "Crossroads with no priority",
           url: "https://i.imgur.com/BNXlBMl.png"
         },
         {
           code: "W.208",
-          content: "Biển báo giao nhau với đường ưu tiên.",
+          content: "Crossroads with priority",
           url: "https://i.imgur.com/tcScj51.png"
         },
         {
           code: "W.209",
-          content: "Biển báo giao nhau có tín hiệu đèn giao thông",
+          content: "Traffic light-controlled intersection",
           url: "https://i.imgur.com/UO1V4lX.png"
         },
         {
           code: "W.210",
-          content: "Biển báo giao nhau với đường sắt có rào chắn",
+          content: "Railway crossing with barriers",
           url: "https://i.imgur.com/X01UzyM.png"
         },
         {
           code: "W.219",
-          content: "Biển báo dốc xuống nguy hiểm.",
+          content: "Downhill slope ahead",
           url: "https://i.imgur.com/jrVEVLz.png"
         },
         {
           code: "W.221b",
-          content: "Biển báo đường có gồ giảm tốc",
+          content: "Road with a simulated reduction in speed",
           url: "https://i.imgur.com/3lqM29z.png"
         },
         {
           code: "W.224",
-          content: "Biển báo đường người đi bộ cắt ngang.",
+          content: "Pedestrian crossing",
           url: "https://i.imgur.com/5pqM2kM.png"
         },
         {
           code: "W.225",
-          content: "Biển báo có trẻ em.",
+          content: "Children present",
           url: "https://i.imgur.com/mg5aTKx.png"
         },
         {
           code: "W.227",
-          content: "Biển báo có công trường",
+          content: "Construction site",
           url: "https://i.imgur.com/vO9LFlX.png"
         },
         {
           code: "W.233",
-          content: "Biển báo có nguy hiểm khác",
+          content: "Other danger",
           url: "https://i.imgur.com/trC1aNz.png"
         },
         {
           code: "W.235",
-          content: "Biển báo có đường đôi",
+          content: "Dual carriageway",
           url: "https://i.imgur.com/FymipbT.png"
         },
         {
           code: "W.245a",
-          content: "Biển báo đi chậm.",
+          content: "Slow down.",
           url: "https://i.imgur.com/ldAO9Bo.png"
         }
       ], {});
